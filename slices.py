823d15846969477e0bcc6a6c124b8ea7f9950391
esik <wowesik@gmail.com>
2023-04-04 23:54:12 +0300

    slices file

---
+++ slices.py
@@ -0,0 +1,14 @@
+# players on my team
+players = ['martin', 'mihail', 'bronto', 'elizabet']
+print('First tree players on my team')
+for player in players[3:]:
+    print(player.title())
+
+# favorite food
+my_foods = ['pizza', 'sandwich', 'carrot cake']
+friend_food = my_foods[:]
+my_foods.('lazania')
+friend_food.append('potato')
+print('My favorite food', my_foods)
+print('\nMy friend favorite food is', friend_food)
+
