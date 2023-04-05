70528c86297fd6750e1ac7de261521b47248e2d1
esik <wowesik@gmail.com>
2023-04-06 00:10:09 +0300

    if

--- if.py
+++ if.py
@@ -1,7 +1,18 @@
-#cars = ['lada', 'suzuki', 'bmw', 'volvo']
-#for car  in cars:
-#    if car == 'bmw':
-#        print('This is',car.upper())
-#    else:
-#        print(car)
+# BMW
+cars = ['lada', 'suzuki', 'bmw', 'volvo']
+for car  in cars:
+    if car == 'bmw':
+        print('This is',car.upper())
+    else:
+        print(car)
 
+# alien_color
+alien_color = input('please input collor green|yellow|red ')
+if alien_color == 'green':
+    print('player get 5 coin')
+elif alien_color == 'yellow':
+    print('player get 10 coin')
+elif alien_color == 'red':
+    print('player get 15 coin')
+else :
+    print('You lose')
